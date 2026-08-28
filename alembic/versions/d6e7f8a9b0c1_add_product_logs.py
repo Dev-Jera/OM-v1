e480e73ff4b658c30d62bafc72e0159f963afa49
ALOYO-BRENDA-OJERA <aloyobrendaojera@gmail.com>
2026-08-28 14:27:06 +0300

    fix: give add_product_logs migration a unique revision ID (d6e7f8a9b0c1)

--- alembic/versions/d6e7f8a9b0c1_add_product_logs.py
+++ alembic/versions/d6e7f8a9b0c1_add_product_logs.py
@@ -1,6 +1,6 @@
 """add product_logs table
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: d6e7f8a9b0c1
 Revises: e5f6a7b8c9d0
 Create Date: 2026-08-25 13:00:00.000000
 
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "a1b2c3d4e5f6"
+revision: str = "d6e7f8a9b0c1"
 down_revision: Union[str, Sequence[str], None] = "e5f6a7b8c9d0"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
